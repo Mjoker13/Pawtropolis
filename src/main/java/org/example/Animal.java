5331d788fabebd2427323daa699b53f3a7c58f4d
MarcoAlemanni <auto13alemanni@gmail.com>
2023-08-04 11:53:08 +0200

    First commit

---
+++ src/main/java/org/example/Animal.java
@@ -0,0 +1,73 @@
+package org.example;
+
+import java.time.LocalDate;
+
+public abstract class Animal {
+    private String name;
+    private String favoriteFood;
+    private int age;
+    private LocalDate entryDate;
+    private double weight;
+    private double height;
+
+    public Animal() {
+    }
+
+    public Animal(String name, String favoriteFood, int age, LocalDate entryDate, double weight, double height) {
+        this.name = name;
+        this.favoriteFood = favoriteFood;
+        this.age = age;
+        this.entryDate = entryDate;
+        this.weight = weight;
+        this.height = height;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getFavoriteFood() {
+        return favoriteFood;
+    }
+
+    public void setFavoriteFood(String favoriteFood) {
+        this.favoriteFood = favoriteFood;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public LocalDate getEntryDate() {
+        return entryDate;
+    }
+
+    public void setEntryDate(LocalDate entryDate) {
+        this.entryDate = entryDate;
+    }
+
+    public double getWeight() {
+        return weight;
+    }
+
+    public void setWeight(double weight) {
+        this.weight = weight;
+    }
+
+    public double getHeight() {
+        return height;
+    }
+
+    public void setHeight(double height) {
+        this.height = height;
+    }
+    
+}
---
+++ src/main/java/org/example/Eagle.java
@@ -0,0 +1,4 @@
+package org.example;
+
+public class Eagle extends Animal{
+}
